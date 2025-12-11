71294df24dfbf81bcfb92a4e8d83afbada5c3a50
Arhipvp <arhipvp@gmail.com>
2025-12-12 01:07:35 +0300

    chore: remove specified file.

--- backend/scripts/populate_test_data.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-
-# Затягивается в контейнере backend, рабочая директория /app
-python manage.py shell < populate_test_data.py
--- backend/scripts/reset_db.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-
-# Запускается внутри контейнера backend, корневая директория уже /app
-
-python manage.py flush --no-input
-python manage.py migrate
