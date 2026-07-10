1fcbcd8eefec7d98fc0b744bb6aad0439bce6c2b
arhipvp <arhipvp@gmail.com>
2026-07-10 19:45:20 +0300

    Fix PostgreSQL trigram index syntax

--- backend/apps/common/indexes.py
+++ backend/apps/common/indexes.py
@@ -1,6 +1,5 @@
-from django.contrib.postgres.indexes import GinIndex, OpClass
+from django.contrib.postgres.indexes import GinIndex
 from django.db import models
-from django.db.models.functions import Upper
 
 
 class PostgresTrigramIndex(GinIndex):
@@ -9,7 +8,8 @@ class PostgresTrigramIndex(GinIndex):
     def __init__(self, field_name: str, *, name: str):
         self.field_name = field_name
         super().__init__(
-            OpClass(Upper(field_name), name="gin_trgm_ops"),
+            fields=[field_name],
+            opclasses=["gin_trgm_ops"],
             name=name,
         )
 
